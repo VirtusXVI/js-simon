6ba1880882a817d7a1cbec48f6568d2668f4f08b
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-16 15:24:20 +0200

    completed functions

--- js/script.js
+++ js/script.js
@@ -3,12 +3,45 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // array
-const numbers = generateNumbers();
+const numbers = generateNumbers(5, 1, 10);
 // alert che mostra i 5 numeri casuali chiamando la funzione
-
-// funzione che genera 5 numeri casuali,non duplicati e li inserisce nell'array
-
-// funzione timeout di 30 secondi nella quale l'utente inserisc i numeri in un prompt
-
+alert(numbers);
+// funzione timeout di 30 secondi
+let clock = setTimeout(gameEnd, 3000);
 // dopo 30 secondi il prompt si chiude e viene visualizzato un messaggio che mostra quanti numeri sono stati indovinati,quali numeri sono stati indovinati e l'array originale che contiene tutti i numeri
 
+
+
+// funzione che verrà eseguita durante i 30 secondi
+function game(numbers){
+    let guessedNumbers = [];
+    for(let i = 0;i < 5;i++){
+        let userNumber = parseInt(prompt("inserisci uno dei numeri tra 1 e 10 che hai visualizzato prima"))
+        if(numbers.includes(userNumber)){
+            guessedNumbers.push(userNumber);
+        }
+    }
+    return guessedNumbers;
+}
+
+// funzione che verrà eseguita dopo 30 secondi dalla chiusura dell'alert
+function gameEnd() {
+    let guessedNumbers = game(numbers);
+    alert(`Complimenti hai indovinato${guessedNumbers.length} numeri su 5, i numeri generati erano:${numbers}`);
+}
+
+// funzione che genera 5 numeri casuali,non duplicati e li inserisce nell'array
+function generateNumbers(n, min, max){
+    let generatedNumbers = [];
+    while(generatedNumbers.length < n){
+        let number = getRndInteger(min, max);
+        if(!generatedNumbers.includes(number)){
+            generatedNumbers.push(number);
+        }
+    }
+    return generatedNumbers;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
