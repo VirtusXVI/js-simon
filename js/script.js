3fcd586b552f0a4b37596acb203aff114b6de6cf
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-16 14:30:54 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Visualizzare in un alert 5 numeri casuali.
+// 30 secondi dopo la chiusura dell'alert, l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// array
+const numbers = generateNumbers();
+// alert che mostra i 5 numeri casuali chiamando la funzione
+
+// funzione che genera 5 numeri casuali,non duplicati e li inserisce nell'array
+
+// funzione timeout di 30 secondi nella quale l'utente inserisc i numeri in un prompt
+
+// dopo 30 secondi il prompt si chiude e viene visualizzato un messaggio che mostra quanti numeri sono stati indovinati,quali numeri sono stati indovinati e l'array originale che contiene tutti i numeri
+
